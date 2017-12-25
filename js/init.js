5bc3f738a6ef48575b9280884105147541dca188
ruhel <ruhelzahidul@gmail.com>
2017-12-26 01:23:04 +0600

    Products page is in progress...

---
+++ js/init.js
@@ -0,0 +1,34 @@
+//Include function
+$("[data-url]").each(function () {
+    var x = $(this).attr('data-url');
+    $(this).load(x).removeAttr("data-url");
+});
+$(document).on('mouseover', '.dropdown', function () {
+        $(this).addClass('open')
+    }
+);
+$(document).on('mouseout', '.dropdown', function () {
+        $(this).removeClass('open')
+    }
+);
+$('.slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+});
+// side nav open
+$(document).on("click", '.thumbs > img', function () {
+    $(this).parents('.product').find('.largeImage > img').attr('src', $(this).attr('src').replace('thumb', 'large'));
+    $(this).addClass("active").siblings().removeClass("active");
+});
+var sourceSwap = function () {
+    var $this = $(this);
+    var newSource = $this.data('hover');
+    $this.data('hover', $this.attr('src'));
+    $this.attr('src', newSource);
+};
+$(function () {
+    $('.largeImage > img').hover(sourceSwap, sourceSwap);
+});
